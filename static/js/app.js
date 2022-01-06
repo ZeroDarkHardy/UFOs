4b905ee8b1155c2dad24100452d978cd3ab90651
Matt Hardy <corporalhardy@gmail.com>
2022-01-06 11:28:13 -0800

    periodic save

---
+++ static/js/app.js
@@ -0,0 +1,45 @@
+// import the data from data.js
+const tableData = data;
+
+// reference the HTML table using d3
+var tbody = d3.select("tbody");
+
+// create funtion for table
+function buildTable(data) {
+    // First, clear out any existing data
+    tbody.html("");
+  
+    // Next, loop through each object in the data
+    // and append a row and cells for each value in the row
+    data.forEach((dataRow) => {
+      // Append a row to the table body
+        let row = tbody.append("tr");
+  
+      // Loop through each field in the dataRow and add
+      // each value as a table cell (td)
+        Object.values(dataRow).forEach((val) => {
+            let cell = row.append("td");
+            cell.text(val);
+            }
+        );
+    });
+}
+
+// create "handleClick" function
+function handleClick() {
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+    if (data) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+    // rebuild the table using the filtered data
+    // @NOTE: if no date was entered, then filteredData will
+    // just be the original tableData
+    buildTable(filteredData);
+}
+
+// attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// build the table when the page loads
+buildTable(tableData);
